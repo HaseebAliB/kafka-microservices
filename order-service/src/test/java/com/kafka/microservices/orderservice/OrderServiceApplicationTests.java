af3d59d97300eee8cbb294349eeca5052dae85c8
haseeb <haseeb.alib@gmail.com>
2026-02-23 22:45:50 +0500

    Dockerize services

--- order-service/src/test/java/com/kafka/microservices/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/kafka/microservices/orderservice/OrderServiceApplicationTests.java
@@ -3,11 +3,26 @@ package com.kafka.microservices.orderservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void healthCheck() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/actuator/health", String.class);
+        assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
+        assertThat(response.getBody()).contains("\"status\":\"UP\"");
+    }
 }
